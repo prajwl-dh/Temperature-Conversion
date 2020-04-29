7c99886412ab6115b8968830d20c1360e7a40b5a
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2020-04-29 16:25:18 -0500

    Initial Commit

---
+++ Temperature Conversion/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Temperature Conversion
+//
+//  Created by Prajwal Dhungana on 4/29/20.
+//  Copyright © 2020 Prajwal Dhungana. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
